21476c434cc1c0e57ebc2c019aaeafdece5305cf
radhakrishnan-s <radhakrishnan460@gmail.com>
2019-07-26 20:39:23 +0530

    last working changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,32 +4,21 @@ import './App.css';
 
 const App: React.FC = () => {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.tsx</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
 <div>
 <nav className="navbar navbar-expand-sm bg-dark navbar-dark border-light">
-  <a className="navbar-brand" href="#"><img src={logo} className="App-logo" alt="logo"/></a>
+  <a className="navbar-brand" href="#"><img src={logo} className="logo" alt="logo"/></a>
 
   {/* <div className="navbar-text text-white">
   The Bloggers Den
-  </div> */}
+  </div> 
   <span className="border-light ">
     <p className="navbar-text bg-dark text-white">Connecting ideas and people – how talk can change our lives!!!</p>
-  </span>
+  </span>*/}
+  <div className="navbar-collapse">
+      <div className="navbar-nav ml-auto">
+          <span className="nav-item text-white">Connecting ideas and people – how talk can change our lives!!!</span>
+      </div>
+    </div>
   </nav>
   <section>test1</section>
   <div className="card card-footer text-muted border-light bg-dark text-white">
