6b13f4d65e952a12e2a319e8982acfa74f1b985e
radhakrishnan-s <radhakrishnan460@gmail.com>
2019-07-28 14:58:06 +0530

    working Modal changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Header from './Header';
 import Description from './Description';
 import Content from './Content';
 import Footer from './Footer';
+import CreateBlogPost from './CreateBlogPost';
 import './App.css';
 
 class App extends React.Component {
@@ -15,6 +16,7 @@ class App extends React.Component {
         <main role="main">
         <Description/>
         <Content/>
+        <CreateBlogPost/>
         </main>
         <footer>
           <Footer/>
